71bf929ece0b642e04421fe67625dca930e57a7a
krashnerd <krishkringle1@gmail.com>
2019-10-27 16:31:14 -0400

    Configured homepage/got css working

--- kkahn_site/homepage/views.py
+++ kkahn_site/homepage/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render_to_response
+
+def index (request):
+    return render(request, 'homepage/homepage.html')
+
+
+# def index(request):
+# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# 	return render(request, "homepage/homepage.html")
