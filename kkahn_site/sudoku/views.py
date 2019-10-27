a4169643cd4361af21a849c0e1e01bf30cbabbcd
krashnerd <krishkringle1@gmail.com>
2019-10-27 16:41:55 -0400

    Got sudoku static files to load

--- kkahn_site/sudoku/views.py
+++ kkahn_site/sudoku/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Sudoku views page")
+    return render(request, "sudoku/SudokuSolver.html")
