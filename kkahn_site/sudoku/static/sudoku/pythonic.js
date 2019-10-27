63948fafbc7709d22d28e28e3ed757c6f0aeb36e
krashnerd <krishkringle1@gmail.com>
2019-10-27 16:32:35 -0400

    Created sudoku django app, moved sudoku files

---
+++ kkahn_site/sudoku/static/sudoku/pythonic.js
@@ -0,0 +1,18 @@
+const range = (start, end = undefined, step = 1) {
+	if end === undefined {
+		step = 1
+		end = start
+		start = 0
+	}
+	var diff = end - start
+	if diff === 0 {
+		return
+	}
+
+	
+
+
+
+	length = 
+	return Array.from(Array(end - start).keys(), x => start + (x * step)	
+}
