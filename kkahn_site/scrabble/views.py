ca4f38ff0247e65b68b86e5f64635cc83067be8e
krashnerd <krishkringle1@gmail.com>
2019-10-29 14:46:28 -0400

    Started adding board view

--- kkahn_site/scrabble/views.py
+++ kkahn_site/scrabble/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+class Square():
+    def __init__(self, bonustype = None):
+        bonuses = dict({
+            "DW": ("Double Word Score", "square-double-word"),
+            "TW": ("Triple Word Score", "square-triple-word"),
+            "DL": ("Double Letter Score", "square-double-letter"),
+            "TL": ("Triple Letter Score", "square-triple-letter"),
+            None: ("", "square"),
+        })
+
+        self.bonustext, self.type = bonuses.get(bonustype, ("", "square"))
+
+
+
+
+
 def index(request):
-    return HttpResponse("Scrabble views page")
+    context = 
+
+    return render(request, "scrabble/scrabble_view.html", context)
 # Create your views here.
