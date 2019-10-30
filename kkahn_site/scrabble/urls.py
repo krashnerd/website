c90b0f2fc7aa8758265a45f147d86343c2a358bb
krashnerd <krishkringle1@gmail.com>
2019-10-30 13:20:38 -0400

    Namespace, pathing issue

--- kkahn_site/scrabble/urls.py
+++ kkahn_site/scrabble/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'scrabble'
+
 urlpatterns = [
     path('', views.index, name='index'),
-]
+	# path('<int:square_id>/', views.display_id, name = 'display_id'),
+	path('<int:square_id>/clicksquare/', views.clicksquare, name = 'clicksquare'),
+ ]
